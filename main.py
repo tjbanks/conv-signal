524b6dc53c4c27dbf153d3bc3cd7d7ff6b728725
Tyler Banks <tyler@tylerbanks.net>
2018-01-09 20:33:25 -0600

    basic functionality

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-from sig2learn import Signal
+from sig2learn import Signals
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from sklearn.preprocessing import MinMaxScaler
+
 
 matfile = "LFP_QW_long_tuning7_synr1.mat"
 matvar = "LFP_array_all_sum_afterHP"
 
-s = Signal().load_mat(matfile,matvar).replace_with_cutout(1000,1200).append_bandpass_filter(20,25).replace_with_hilbert_transform().signals[1].testprint()
+s = Signals().load_mat(matfile,matvar).set_name('LFP1').cut(1000,4000) \
+    .append_bandpass(2,10).to_hilbert() \
+    .append_bandpass(11,20).to_hilbert() \
+    .append_bandpass(21,30).to_hilbert() \
+    .append_bandpass(31,40).to_hilbert() \
+    .append_bandpass(41,50).to_hilbert() \
+    .append_bandpass(51,63).to_hilbert() \
+    .append_bandpass(64,84).to_hilbert() \
+    .remove_original_signal()
+    
+scaler = MinMaxScaler(feature_range=(0,1))
+    
+nps = s.to_np_signal().scale(scaler).to_supervised_series_3D(300, 20)
+
+print nps
+
+print len(s.get_signals())
+
+def plot_signal(x, n_per_second, signame, newPlot=True):
+    
+    if newPlot:
+        plt.figure(figsize=(15,10))
+        plt.clf()
+        
+    fs = x.size
+    t = np.linspace(0, float(fs)/n_per_second, fs, endpoint=False)
+    plt.plot(t, x, label=signame)
+    plt.legend()
+    plt.xlabel('Time (seconds)')
+
+plt.figure(figsize=(15,10))
+for sig in s.get_signals():
+    plot_signal(sig.get_np_arr(), 1000, sig.get_name(), newPlot=False)
+#plot_signal(s.get_signals()[1].get_np_arr(), 1000, s.get_signals()[1].get_name())
--- sig2learn.py
+++ sig2learn.py
@@ -2,22 +2,14 @@
 __author__ = "Tyler Banks"
 __version__ = "1.0.0"
 
-import sys, math
-
 from scipy.signal import butter, lfilter, hilbert
 import scipy.io as sio
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
-import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
 
 ##Input
-class Signal:
-
-    sig = None
+class Signals(object):
 
     def __init__(self):
         return
@@ -35,20 +27,30 @@ class Signal:
         return MultiSignal(SingleSignal(matx))
 
 ##Intermediary
-class SingleSignal:
+class SingleSignal(object):
 
-    np_arr = None
+    #np_arr = None
 
     def __init__(self, numpy_arr):
         self.np_arr = numpy_arr
+        self.readable_name = ''
         return 
 
     def testprint(self):
         print self.np_arr
 
+    def get_np_arr(self):
+        return self.np_arr
+    
+    def get_name(self):
+        return self.readable_name
+    
     def replace_np_arr(self, npa):
         self.np_arr = npa
         return
+    
+    def set_name(self, name):
+        self.readable_name = name
 
     def get_cutout(self,n_start,n_end):
         return self.np_arr[n_start:n_end]
@@ -62,13 +64,17 @@ class SingleSignal:
 
 
 ##Intermediary
-class MultiSignal:
+class MultiSignal(object):
 
-    signals = []
+    #signals = []
 
     def __init__(self, singlesignal):
+        self.signals = [] 
         self.append_signal(singlesignal)
         return
+    
+    def get_signals(self):
+        return self.signals
 
     def append_signal(self, singlesignal):
         self.signals.append(singlesignal)
@@ -79,43 +85,78 @@ class MultiSignal:
         return self
 
     def remove_original_signal(self):
-        remove_signal(self, 0)
+        self.remove_signal(0)
         return self
-
-    def append_bandpass_filter(self, low, high, order=2, index=0):
-        s = SingleSignal(self.signals[index].get_bandpass_filter(low, high, order))
-        self.append_signal(s)
+    
+    def set_name(self, name):
+        self.signals[-1].set_name(name)
         return self
-
-    def replace_with_bandpass_filter(self):
-        npa = self.signals[-1].get_bandpass_filter(low, high, order)
-        self.signals[-1].replace_np_arr(npa)
-        return self
-
-    def replace_with_cutout(self,n_start,n_end):
+    
+    def cut(self,n_start,n_end):
         npa = self.signals[-1].get_cutout(n_start,n_end)
         self.signals[-1].replace_np_arr(npa)
         return self
 
-    def replace_with_hilbert_transform(self):
+    def append_bandpass(self, low, high, order=2, index=0):
+        s = SingleSignal(self.signals[index].get_bandpass_filter(low, high, order))
+        s.set_name('%s Filter Band %d - %d Hz'%(self.signals[index].get_name(), low, high))
+        self.append_signal(s)
+        return self
+
+    def to_bandpass(self, low, high, order=2):
+        s = self.signals[-1]
+        npa = s.get_bandpass_filter(low, high, order)
+        s.set_name('Filter Band %d - %d Hz'%(low, high))
+        self.signals[-1].replace_np_arr(npa)
+        return self
+    
+    def append_hilbert(self, index=-1):
+        s = SingleSignal(self.signals[index].get_hilbert_transform())
+        s.set_name('%s Hilbert Transform'%(self.signals[index].get_name()))
+        self.append_signal(s)
+        return self
+
+    def to_hilbert(self):
+        s = self.signals[-1]
         npa = self.signals[-1].get_hilbert_transform()
+        s.set_name('%s Hilbert Transform'%(s.get_name()))
         self.signals[-1].replace_np_arr(npa)
         return self
 
-    def to_numpy():
-        return
+    def to_np_signal(self):
+        bandsets = []
+        for s in self.signals:
+            bandsets.append(s.get_np_arr())
+        return NP_MultiSignal(np.array(bandsets))
 
-    def scale():
+class NP_MultiSignal(object):
+    
+    #np_arr = []
+    
+    def __init__(self, npa):
+        self.np_arr = npa
         return
+    
+    def to_np(self):
+        return self.np_arr
+    
+    def scale(self, scaler):
+        self.np_arr = self.np_arr.astype('float32')
+        self.np_arr = self.np_arr.transpose()
+        self.np_arr = scaler.fit_transform(self.np_arr)
+        return self
 
-    def to_supervised_series_2D():
+    def to_supervised_series_2D(self):
         """Returns the selected index time series, last element will be goal"""
         return
 
-    def to_supervised_series_3D():
+    def to_supervised_series_3D(self, look_back, future_element):
         """Returns all indexes as time series"""
-        return
-
+        out = series_to_supervised_CNNData(self.np_arr, n_in=look_back, n_out=future_element, dropnan=True)
+        out = out.values
+        return out
+    
+    
 #================ AUX FUNCTIONS ================
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
